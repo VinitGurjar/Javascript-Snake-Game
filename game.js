3637425ef12621d4274e8b610b69f0e8e4737895
Sherman Rofeman <skippyr.developer@gmail.com>
2023-02-13 17:08:38 -0300

    feat: add script for game over window.

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ import {
 } from "./snake.js";
 import { update as updateFood, draw as drawFood } from "./food.js";
 import { outsideGrid } from "./grid.js";
+import { openGameOverWindow } from "./game_over_window.js";
 
 let lastRenderTime = 0;
 let gameOver = false;
@@ -24,12 +25,10 @@ function restoreHighestScore() {
 
 function main(currentTime) {
   if (gameOver) {
-    if (confirm("You lost. Press ok to restart.")) {
-      if (snakeBody.length > getHighestScore()) {
-        localStorage.setItem("highestScore", snakeBody.length);
-      }
-      window.location = "/";
+    if (snakeBody.length > getHighestScore()) {
+      localStorage.setItem("highestScore", snakeBody.length);
     }
+    openGameOverWindow(snakeBody.length);
     return;
   }
 
